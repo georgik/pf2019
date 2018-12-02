52218a7fb0f06fe7e8e2fe395365d374b5834fdf
Juraj Michalek <juraj.michalek@gmail.com>
2018-12-02 22:10:14 +0100

    check whether terrain is walkable

--- js/game.js
+++ js/game.js
@@ -77,30 +77,38 @@ let game = new Vue({
     store,
     data: {},
     methods: {
+        isWalkable: function(coordX, coordY) {
+            let tileX = coordX / this.$store.state.tile.width;
+            let tileY = coordY / this.$store.state.tile.height;
+            let tile = this.$store.state.levelMap[tileY][tileX];
+            return (tile === " ");
+        },
         mouseClicked: function (event) {
             let avatar = this.$store.state.actors[0];
             let deltaX = event.clientX - avatar.x - 32;
             let deltaY = event.clientY - avatar.y - 32;
-            let maxX = this.$store.state.level.width - this.$store.state.tile.width;
-            let maxY = this.$store.state.level.height - this.$store.state.tile.height;
+            let tileWidth = this.$store.state.tile.width;
+            let tileHeight = this.$store.state.tile.height;
+            let maxX = this.$store.state.level.width - tileWidth;
+            let maxY = this.$store.state.level.height - tileHeight;
 
             if (Math.abs(deltaX) > Math.abs(deltaY)) {
                 if (deltaX > 0) {
-                    if (avatar.x < maxX) {
+                    if ((avatar.x < maxX) && (this.isWalkable(avatar.x + tileWidth, avatar.y))) {
                         store.commit('moveRight');
                     }
                 } else {
-                    if (avatar.x > 0) {
+                    if ((avatar.x > 0) && (this.isWalkable(avatar.x - tileWidth, avatar.y))) {
                         store.commit('moveLeft');
                     }
                 }
             } else {
                 if (deltaY > 0) {
-                    if (avatar.y < maxY) {
+                    if ((avatar.y < maxY) && (this.isWalkable(avatar.x, avatar.y + tileHeight))) {
                         store.commit('moveDown');
                     }
                 } else {
-                    if (avatar.y > 0) {
+                    if ((avatar.y > 0) && (this.isWalkable(avatar.x, avatar.y - tileHeight))) {
                         store.commit('moveUp');
                     }
                 }
