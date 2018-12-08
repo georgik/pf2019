557a107d007a072d044f99bf4771b69edd087d3f
Juraj Michalek <juraj.michalek@gmail.com>
2018-12-08 22:50:54 +0100

    dynamic generated level selector

--- js/level-selector.js
+++ js/level-selector.js
@@ -2,6 +2,11 @@
 let levelSelector = Vue.component('LevelSelector', {
     template: '#level-selector-template',
     store,
+    computed: {
+        levels() {
+            return this.$store.state.levels;
+        }
+    },
     methods: {
     }
 });
