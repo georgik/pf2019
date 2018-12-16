076a12decda8d18c65388cba219cdddf99fa0066
Juraj Michalek <juraj.michalek@ysoft.com>
2018-12-16 18:45:41 +0100

    add 3 more levels

--- js/levels.js
+++ js/levels.js
@@ -25,7 +25,42 @@ const datadisc = [
             "w xox w",
             "w  x  w",
             "wwwwwww"]
+    },
+    {
+        levelMap: [
+            "wwwwwwwwwwww",
+            "w        aw",
+            "w o     xw",
+            "wwowo  xw",
+            "w  x   w",
+            "wwwwwww"]
+    },
+    {
+        levelMap: [
+            "wwwwwwwwwwww",
+            "w  x    xa w",
+            "w wxw  wxw w",
+            "w  x    x  w",
+            "w     x    w",
+            "w     w    w",
+            "w   o   o  w",
+            "w   ooooo  w",
+            "w          w",
+            "wwwwwwwwwwww"]
+    },
+    {
+        levelMap: [
+            "wwwwwwwwwwww",
+            "w     wwx aw",
+            "w       x  w",
+            "w     wwx  w",
+            "w  wwwwwwwww",
+            "w          w",
+            "w          w",
+            "wwwwwwwww  w",
+            "w  o    oo w",
+            "w          w",
+            "wwwwwwwwwwww"]
     }
-
 ];
 
