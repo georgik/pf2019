cbf7b88f56549ffb98a2e9c566fc6bfd6449bfc6
Juraj Michalek <juraj.michalek@ysoft.com>
2018-12-27 14:01:00 +0100

    remove unused attribute image

--- js/store.js
+++ js/store.js
@@ -20,11 +20,6 @@ let store = new Vuex.Store({
     mutations: {
         updateName(state, { actor, name}) {
             actor.name = name;
-            if (actor.name === "mfd") {
-               actor.image = "bulb-off";
-            } else {
-                actor.image = "bulb-on";
-            }
         },
         moveVector(state, { actor, vectorX, vectorY }) {
             actor.x += vectorX * state.tile.width;
@@ -44,7 +39,6 @@ let store = new Vuex.Store({
                     if (tile === "a") {
                         state.actors.unshift({
                             name: 'avatar',
-                            image: 'robot',
                             x: x * tileWidth,
                             y: y * tileHeight
                         });
@@ -53,7 +47,6 @@ let store = new Vuex.Store({
                     } else if (tile === "o") {
                         state.actors.push( {
                             name: 'mfd',
-                            image: 'bulb-off',
                             x: x * tileWidth,
                             y: y * tileHeight
                         });
