68cd86adec9904b690eb11bf09852ebcfc03444d
Juraj Michalek <juraj.michalek@gmail.com>
2019-01-06 15:40:53 +0100

    move avatar on mouse click

--- js/game.js
+++ js/game.js
@@ -17,21 +17,62 @@ const store = new Vuex.Store({
                 y: 2
             }
         ]
+    },
+    mutations: {
+        moveObject(state, { gameObject, vectorX, vectorY }) {
+            gameObject.x += vectorX;
+            gameObject.y += vectorY;
+        },
     }
 });
 
 Vue.component('playground', {
-        template: '#playground-template',
-        store,
-        props: {},
-        computed: {
-            levelMap() {
-                return this.$store.state.levelMap;
-            },
-            gameObjects() {
-                return this.$store.state.gameObjects;
+    template: '#playground-template',
+    store,
+    props: {},
+    computed: {
+        levelMap() {
+            return this.$store.state.levelMap;
+        },
+        gameObjects() {
+            return this.$store.state.gameObjects;
+        }
+    },
+    methods: {
+        moveGameObject: function(gameObject, vectorX, vectorY) {
+            store.commit("moveObject", { gameObject, vectorX, vectorY });
+        },
+        mouseClicked: function(event) {
+            let rect = event.currentTarget.getBoundingClientRect();
+            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            let top = rect.top + scrollTop;
+            let left = rect.left + scrollLeft;
+
+            let avatar = this.$store.state.gameObjects[0];
+            let deltaX = (event.clientX - left)/64 - avatar.x;
+            let deltaY = (event.clientY - top)/64 - avatar.y;
+
+            // Do not move in case of click inside the icon of avatar
+            if ((deltaX === 0) && (deltaY === 0)) {
+                return;
+            }
+
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                if (deltaX > 0) {
+                    this.moveGameObject(avatar, 1, 0);
+                } else {
+                    this.moveGameObject(avatar, -1, 0);
+                }
+            } else {
+                if (deltaY > 0) {
+                    this.moveGameObject(avatar, 0, 1);
+                } else {
+                    this.moveGameObject(avatar, 0, -1);
+                }
             }
         }
+    }
 });
 
 var game = new Vue({
